f88f4bb9a151c3043bb6ae7fbd00b7715215c48d
emp-temp <emptemp0000@gmail.com>
2024-09-19 14:42:23 +0900

    feat: 🎸 開いた時にlaraonvimが見えるように変更

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,10 +1,24 @@
 vim.g.nightflyTransparent = true
 
 return {
-  {
-    "bluz71/vim-nightfly-colors",
-    name = "nightfly",
-    lazy = false,
-    priority = 1000,
-  },
+	{
+		"bluz71/vim-nightfly-colors",
+		name = "nightfly",
+		lazy = false,
+		priority = 1000,
+	},
+	{
+		"marko-cerovac/material.nvim",
+		opts = {
+			contrast = {
+				terminal = true, -- Enable contrast for the built-in terminal
+				sidebars = true, -- Enable contrast for sidebar-like windows ( for example Nvim-Tree )
+				floating_windows = true, -- Enable contrast for floating windows
+				cursor_line = false, -- Enable darker background for the cursor line
+				lsp_virtual_text = true, -- Enable contrasted background for lsp virtual text
+				non_current_windows = false, -- Enable contrasted background for non-current windows
+				filetypes = {}, -- Specify which filetypes get the contrasted (darker) background
+			},
+		},
+	},
 }
